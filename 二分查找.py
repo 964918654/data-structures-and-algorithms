7d60e63416a2180a9b3a28e86a090cfe201cd1c7
964918654 <964918654@qq.com>
2018-11-16 08:35:30 +0800

    day3

--- 二分查找.py
+++ 二分查找.py
@@ -7,24 +7,49 @@ def A(t,n):
 
     return False
 
-t = [1,2,3,4,5,6,7,8]
+#t = [1,2,3,4,5,6,7,8]
 
-print(A(t,5))
+#print(A(t,5))
 
 #二分查找
 def B(t,n):
     left = 0
     right = len(t)-1
     while left<=right:
-        m = (t[left] + t[right])/2
-        if m<n:
-            left = left + 1
-        elif m >n:
-            right = right - 1
+        m = int((left + right) / 2)
+        if t[m]<n:
+            left = m + 1
+        elif t[m] >n:
+            right = m - 1
         else:
             return True
     return False
-print(B(t,9))
+#print(B(t,5))
+#二分查找的时间复杂度为O(logN)
+#使用递归方法进行二分查找
+def C(t,n):
+    left = 0
+    right = len(t)-1
+    if left<=right:
+        mid = int((left+right)/2)
+        if t[mid]<n:
+            C(t[mid+1:],n)
+        elif t[mid]>n:
+            C(t[:mid-1],n)
+        else:
+            return True
 
+    return False
 
+#print(C(t,9))
+import time
+array = list(range(100000000))
+t1 = time.time()
+A(array,100000001)
+t2 = time.time()
+print('线性查找:',t2-t1)
 
+t3 = time.time()
+B(array, 100000001)
+t4 = time.time()
+print('二分查找：', t4 - t3)
